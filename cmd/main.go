be448313ac69aef1796363d2b92763b9d850287d
ikeyu0806 <ikeyu0806@gmail.com>
2023-08-11 18:09:39 +0900

    install colly

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Scraping Start.")
+	url := "https://b.hatena.ne.jp/hotentry/it"
+
+	c := colly.NewCollector()
+
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		fmt.Println("Title:", e.Text)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting URL:", r.URL.String())
+	})
+
+	c.Visit(url)
 }
