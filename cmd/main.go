820efb46e0e0c44b7c289d3d085cbb8031bf9ae5
ikeyu0806 <ikeyu0806@gmail.com>
2023-08-11 18:45:57 +0900

    create sites package

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/gocolly/colly/v2"
-)
+import "goroutine-scraping-cli/sites"
 
 func main() {
-	fmt.Println("はてなITホットエントリー")
-	url := "https://b.hatena.ne.jp/hotentry/it"
-
-	c := colly.NewCollector()
-
-	c.OnHTML(".entrylist-contents-main", func(e *colly.HTMLElement) {
-		title := e.ChildText("h3")
-		link := e.ChildAttr("a", "href")
-		fmt.Printf("タイトル: %s\n", title)
-		fmt.Printf("リンク: %s\n\n", link)
-	})
-
-	c.Visit(url)
+	sites.ScrapingHatenaItHotentry()
 }
---
+++ sites/hatena_it.go
@@ -0,0 +1,23 @@
+package sites
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func ScrapingHatenaItHotentry() {
+	fmt.Println("はてなITホットエントリー")
+	url := "https://b.hatena.ne.jp/hotentry/it"
+
+	c := colly.NewCollector()
+
+	c.OnHTML(".entrylist-contents-main", func(e *colly.HTMLElement) {
+		title := e.ChildText("h3")
+		link := e.ChildAttr("a", "href")
+		fmt.Printf("タイトル: %s\n", title)
+		fmt.Printf("リンク: %s\n\n", link)
+	})
+
+	c.Visit(url)
+}
