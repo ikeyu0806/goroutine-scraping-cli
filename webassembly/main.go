f74ebc5d5b6c9665e0ea41cc91c00971d992a075
ikeyu0806 <ikeyu0806@gmail.com>
2023-08-13 09:42:07 +0900

    add webserver container, webassembly code

---
+++ webassembly/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"syscall/js"
+)
+
+func fetchContent(this js.Value, p []js.Value) interface{} {
+	url := "https://www.google.com"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	content := string(body)
+	js.Global().Get("console").Call("log", "Fetched content:", content)
+	return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("fetchContent", js.FuncOf(fetchContent))
+}
+
+func main() {
+	c := make(chan struct{}, 0)
+
+	registerCallbacks()
+
+	<-c
+}
